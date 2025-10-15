a8a3ece61f09e8f44c71f031f36327fb56ab7f3e
Kanadesisido <takasuyari@gmail.com>
2025-10-15 23:38:18 +0900

    create: model

--- backend/model/file_storage.go
+++ backend/model/file_storage.go
@@ -10,3 +10,19 @@ type FileStorage struct {
     Checksum   *string   `gorm:"size:128"`
     UploadedAt time.Time
 }
+
+type FileStorageInsItem struct {
+    ItemID     string    `gorm:"type:char(36);primaryKey"`
+    StorageKey string    `gorm:"size:255;not null"`
+    Checksum   *string   `gorm:"size:128"`
+    UploadedAt time.Time
+    ID        string     `gorm:"type:char(36);primaryKey"`
+    Name      string     `gorm:"size:255;not null"`
+    Type      ItemType   `gorm:"type:enum('file','folder');not null"`
+    ParentID  *string    `gorm:"type:char(36);index"`
+    OwnerID   string     `gorm:"type:char(36);not null;index"`
+    MimeType  *string    `gorm:"size:100"`
+    SizeBytes *int64
+    CreatedAt time.Time
+    UpdatedAt time.Time
+}
