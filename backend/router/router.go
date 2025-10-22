32355c0163991cd21e7a229df0f3daf0e110ea34
Kanadesisido <takasuyari@gmail.com>
2025-10-22 15:04:47 +0900

    improve: router - change to Interface depend

--- backend/router/router.go
+++ backend/router/router.go
@@ -7,38 +7,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func NewRouter(_handler *handler.Handler) *gin.Engine {
+func NewItemsRouter(_itemsHandler handler.ItemsHandler, _userHandler handler.UserHandler) *gin.Engine {
 
 	router := gin.Default()
 	router.Use(middleware.CorsMiddleWare())
 
 	// Routes
 	auth := router.Group("/auth")
-	auth.POST("/login", _handler.LoginHandler)		// "/auth/login"
-	auth.POST("/signup", _handler.SignUpHandler)	// "/auth/signup"
+	auth.POST("/login", _userHandler.LoginHandler)   // "/auth/login"
+	auth.POST("/signup", _userHandler.SignUpHandler) // "/auth/signup"
 
 	perm := router.Group("/s")
 	perm.Use(middleware.Auth)
 	{
-		perm.GET("/rootName", _handler.RootNameHandler)	// "/s/rootName"
+		perm.GET("/rootName", _userHandler.RootNameHandler) // "/s/rootName"
 
 		storage := perm.Group("/:folderId")
 
-		storage.GET("/", _handler.FolderHandler)			// "/:folderId/" フォルダの内容を表示する
-		
+		storage.GET("/", _itemsHandler.FileHandler) // "/:folderId/" フォルダの内容を表示する
+
 		file := storage.Group("/file")
 		{
-			file.POST("/upload", _handler.CreateFileHandler)	// "/:folderId/file/upload" ファイルアップロード
-			file.GET("/:id", _handler.FileHandler)		// "/:folderId/file/:id" ファイルダウンロード
+			file.POST("/upload", _itemsHandler.CreateFileHandler) // "/:folderId/file/upload" ファイルアップロード
+			file.GET("/:id", _itemsHandler.FileHandler)           // "/:folderId/file/:id" ファイルダウンロード
 		}
 
 		folder := storage.Group("/folder")
 		{
-			folder.POST("/create", _handler.CreateFolderHandler)	// "/:folderId/folder/create"
+			folder.POST("/create", _itemsHandler.CreateFolderHandler) // "/:folderId/folder/create"
 		}
-	
+
 	}
 
 	return router
 }
-
